d83a1adc281062a80b5ef42d0db83a10dfa55c6f
Mikron <mikronia@gmail.com>
2015-12-17 20:50:13 +0100

    Working routing

--- config/web.php
+++ config/web.php
@@ -37,6 +37,9 @@ $config = [
                 ],
             ],
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+        ],
         'db' => require(__DIR__ . '/db.php'),
     ],
     'params' => $params,
